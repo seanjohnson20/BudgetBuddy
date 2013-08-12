69432ad0e244c21263ccf596deab8278b6f0a9c7
Sean-Hdrive <seanjohnson@gmail.com>
2013-08-12 09:16:39 -0700

    test commit

--- run.py
+++ run.py
@@ -3,8 +3,6 @@ from routes import app
 from flask import Flask
 from waitress import serve
 
-
-    
 if __name__ == "__main__":
     app.run(debug=True)
     #serve(app, host='0.0.0.0', port='5000')   
