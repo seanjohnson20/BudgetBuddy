3cf0e8e7e5bae96fb421413cd684dc83399c9ce7
Sean Johnson <seanjohnson20@gmail.com>
2013-09-20 13:42:37 -0700

    remove .1 files

--- config.1.py
+++
@@ -1,8 +0,0 @@
-import os
-from datetime import timedelta
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-CSRF_ENABLED = True
-SECRET_KEY = 'LJyyfy8A2MQEI9GynmXkCSVlXIRPUp76'
-DEBUG = True
-PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
--- db_create.1.py
+++
@@ -1,3 +0,0 @@
-from database import init_db
-
-init_db()
